161a1682a0d8e640b712ae89f199f2f7746a1106
adilkadivala <adilkadivala560@gmail.com>
2025-05-19 20:11:58 +0530

    strting covering week-2

--- webdev/week-1/js/day-1.js
+++ webdev/week-1/js/day-1.js
@@ -4,5 +4,71 @@ function sum(a, b) {
   return a + b;
 }
 
-console.log(sum(5, 6));
+// console.log("Answer is ::", sum(5, 6));
 
+// array and objects
+
+let arr = [
+  "kadi",
+  20,
+  {
+    name: "adil Kadival",
+    age: 20,
+    cities: [
+      "Lonavala",
+      "Bombay Central",
+      {
+        country: "india",
+        city: "Bombay",
+        publicPlaces: [
+          "Juhu",
+          "Gate Of India",
+          "Chopati",
+          "Merin-Lines",
+          { historiaclPlaces: "Tag Hotel" },
+        ],
+      },
+    ],
+  },
+];
+
+// console.log(arr);
+// console.log("\n");
+// console.log(...arr);
+// console.log("\n");
+// console.dir(structuredClone(arr), { depth: null });
+
+// assignment
+
+function check(users) {
+  let newUser = [];
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].age > 18 && users[i].gender === "male") {
+      newUser.push(users[i]);
+    }
+  }
+
+  return newUser;
+}
+
+const users = [
+  {
+    name: "Jhon",
+    age: 18,
+    gender: "male",
+  },
+  {
+    name: "sofia",
+    age: 22,
+    gender: "female",
+  },
+  {
+    name: "Jemmy",
+    age: 23,
+    gender: "male",
+  },
+];
+
+const verify = check(users);
+console.log(verify);
---
+++ webdev/week-2/async/index.js
@@ -0,0 +1,1 @@
+// try to promisified version of setTimeout..
