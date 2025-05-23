37e631c51e30715f86c2464bcc0830e2d44b0e9d
adilkadivala <adilkadivala560@gmail.com>
2025-05-23 11:36:48 +0530

    assignments started

---
+++ webdev/week-2/assignments/index.js
@@ -0,0 +1,33 @@
+// anagram
+
+function isAnagram(str1, str2) {
+  let first_str = str1.toLowerCase().split("").sort().join("");
+  let second_str = str2.toLowerCase().split("").sort().join("");
+
+  if (first_str === second_str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let result = isAnagram("adil", "dail");
+console.log(result);
+
+// large value
+function findLarge(arr) {
+  let large = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > large) {
+      large = arr[i];
+    }
+  }
+  return large;
+}
+
+let arrNum = [5, 3, 1, 9];
+const isLarge = findLarge(arrNum);
+console.log(isLarge);
+
+
+// 
