304c1ab6f31aa74bd42caeede310c0aba5bc13db
adilkadivala <adilkadivala560@gmail.com>
2025-05-26 22:17:57 +0530

    week-3 started

---
+++ webdev/week-3/index.js
@@ -0,0 +1,15 @@
+// DOM -- document object model is a programming interface for web documents,
+
+// for fetcching element
+
+// querySelector  // css
+// querySelectAll // css
+// getElementById  //
+// getElementByClassName
+// getElementsByClassName
+// getElementsByTagName
+
+// let fetch body
+const print = document.getElementsByTagName("body")[0];
+console.log("body fetched", print);
+
