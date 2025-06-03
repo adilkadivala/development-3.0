543f37fbe69131cd1bf7ffd444ef4f0305118aec
adilkadivala <adilkadivala560@gmail.com>
2025-06-03 16:51:47 +0530

    middleware cleared

--- webdev/week-4/index.js
+++ webdev/week-4/index.js
@@ -1,1 +1,32 @@
 // week-4 for Node.js
+
+// backend status
+
+// middle-ware ::
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// middleware
+function isAdult(req, res, next) {
+  const age = req.query.age;
+  console.log(age);
+  if (age > 18) {
+    next();
+  } else {
+    res.status(411).json({ msg: "sorry! you are not old enough" });
+  }
+}
+
+app.get("/vote", isAdult, async (req, res) => {
+  try {
+    res.status(200).json({ msg: "You are eligibil for voting" });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
