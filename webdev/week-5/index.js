2c2e21618cb8daa2eaf0da455045001ba42325bc
adilkadivala <adilkadivala560@gmail.com>
2025-06-11 23:20:17 +0530

    request explored

--- webdev/week-5/index.js
+++ webdev/week-5/index.js
@@ -8,6 +8,13 @@ it can
 
 */
 
+// req object propperties
+/*
+    req.url = will give on which route request has come 
+    req.path = will give on which route request has come 
+    req.originalUrl = will give on which route request has come 
+*/
+
 import express from "express";
 
 const app = express();
@@ -18,17 +25,44 @@ const port = 3000;
 
 let requestCount = 0;
 
+// req. counter middleware
 function trackReq(req, res, next) {
   requestCount = requestCount + 1;
   console.log(`total number of requests = ${requestCount} `);
   next();
 }
 
-app.get("/", trackReq, (req, res) => {
+// req. modifying
+function modifyReq(req, res, next) {
+  req.name = "youTheDev";
+  next();
+}
+
+// ending req. for a reasone
+
+function blockTheUser(req, res, next) {
+  if (requestCount < 5) {
+    res.send("You are not able to see welcome message, refresh 5 times");
+    req.emit();
+  }
+  next();
+}
+
+//
+function logIncomingReq(req, res, next) {
+  console.log("method is " + req.method);
+  console.log(new Date().toLocaleTimeString());
+  console.log("request comes form " + req.host);
+  next();
+}
+
+// default route
+app.get("/", trackReq, modifyReq, logIncomingReq, (req, res) => {
   res.send("hey your welcome");
   req.emit();
 });
 
+// server port
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
